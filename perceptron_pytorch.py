7e5b6109a93930a598b2c35a58ab40e32d7e5bc4
Damidas0 <damien.laqueuvre@gmail.com>
2024-09-06 09:02:27 +0200

    Premier commit

--- perceptron_pytorch.py
+++ perceptron_pytorch.py
@@ -1,13 +1,11 @@
 # coding: utf8
 # !/usr/bin/env python
 # ------------------------------------------------------------------------
-# Perceptron en pytorch (en utilisant les outils de Pytorch)
+# Perceptron en pytorch (en utilisant juste les tenseurs)
 # Écrit par Mathieu Lefort
 #
 # Distribué sous licence BSD.
 # ------------------------------------------------------------------------
-
-import gzip,numpy,torch
 
 import gzip, numpy, torch
     
@@ -18,37 +16,43 @@ if __name__ == '__main__':
 	
 	# on lit les données
 	((data_train,label_train),(data_test,label_test)) = torch.load(gzip.open('mnist.pkl.gz'))
-	# on crée les lecteurs de données
-	train_dataset = torch.utils.data.TensorDataset(data_train,label_train)
-	test_dataset = torch.utils.data.TensorDataset(data_test,label_test)
-	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 	# on initialise le modèle et ses poids
-	model = torch.nn.Linear(data_train.shape[1],label_train.shape[1])
-	torch.nn.init.uniform_(model.weight,-0.001,0.001)
-	# on initiliase l'optimiseur
-	loss_func = torch.nn.MSELoss(reduction='sum')
-	optim = torch.optim.SGD(model.parameters(), lr=eta)
+	w = torch.empty((data_train.shape[1],label_train.shape[1]),dtype=torch.float)
+	b = torch.empty((1,label_train.shape[1]),dtype=torch.float)
+	torch.nn.init.uniform_(w,-0.001,0.001)
+	torch.nn.init.uniform_(b,-0.001,0.001)
 
+	nb_data_train = data_train.shape[0]
+	nb_data_test = data_test.shape[0]
+	indices = numpy.arange(nb_data_train)
 	for n in range(nb_epochs):
-		# on lit toutes les données d'apprentissage
-		for x,t in train_loader:
+		# on mélange les (indices des) données
+		numpy.random.shuffle(indices)
+		# on lit toutes les (indices des) données d'apprentissage
+		for i in range(0,nb_data_train,batch_size):
+			# on récupère les entrées
+			x = data_train[indices[i:i+batch_size]]
 			# on calcule la sortie du modèle
-			y = model(x)
+			y = torch.mm(x,w)+b
+			# on regarde les vrais labels
+			t = label_train[indices[i:i+batch_size]]
 			# on met à jour les poids
-			loss = loss_func(t,y)
-			loss.backward()
-			optim.step()
-			optim.zero_grad()
-			
+			grad = (t-y)
+			w += eta * torch.mm(x.T,grad)
+			b += eta * grad.sum(axis=0)
+
 		# test du modèle (on évalue la progression pendant l'apprentissage)
 		acc = 0.
 		# on lit toutes les donnéees de test
-		for x,t in test_loader:
+		for i in range(nb_data_test):
+			# on récupère l'entrée
+			x = data_test[i:i+1]
 			# on calcule la sortie du modèle
-			y = model(x)
+			y = torch.mm(x,w)+b
+			# on regarde le vrai label
+			t = label_test[i:i+1]
 			# on regarde si la sortie est correcte
 			acc += torch.argmax(y,1) == torch.argmax(t,1)
 		# on affiche le pourcentage de bonnes réponses
-		print(acc/data_test.shape[0])
+		print(acc/nb_data_test)
