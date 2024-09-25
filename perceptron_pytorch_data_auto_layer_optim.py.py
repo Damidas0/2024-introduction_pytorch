dd3f44a334e59c55c39aa3a391ff1cdb45d05ec9
Damien Laqueuvre <damien.laqueuvre@gmail.com>
2024-09-25 18:37:04 +0200

    maj

--- perceptron_pytorch_data_auto_layer_optim.py.py
+++ perceptron_pytorch_data_auto_layer_optim.py.py
@@ -7,9 +7,12 @@
 # Distribué sous licence BSD.
 # ------------------------------------------------------------------------
 
+import time
 from matplotlib import pyplot as plt
 import gzip, numpy, torch, matplotlib
 from shallow_network import *
+from torch.utils.data import random_split
+
 
 def train_return(batch_size = 5, nb_epochs = 3, eta = 0.00001, hidden_neurons = 64) :
 	"""Entraine le réseau de neuronnes et renvoi l'évolution de l'accuracy au cours des epochs en fonction des paramètres
@@ -20,14 +23,44 @@ def train_return(batch_size = 5, nb_epochs = 3, eta = 0.00001, hidden_neurons = 
 		eta (float, optional): Taux d'apprentissage. 
 		hidden_neurons (int, optional): Nombre de neuronne de la couche cachée.
 	"""
- 
-	# on lit les données
-	((data_train,label_train),(data_test,label_test)) = torch.load(gzip.open('mnist.pkl.gz'))
-	# on crée les lecteurs de données
-	train_dataset = torch.utils.data.TensorDataset(data_train,label_train)
-	test_dataset = torch.utils.data.TensorDataset(data_test,label_test)
+	
+
+	# Charger les données
+	((data_train, label_train), (data_test, label_test)) = torch.load(gzip.open('mnist.pkl.gz'))
+
+	# Créer le dataset complet
+	full_train_dataset = torch.utils.data.TensorDataset(data_train, label_train)
+	
+
+	# Définir le pourcentage pour la validation
+	val_split = 0.2
+	num_train = len(full_train_dataset)
+	num_val = int(num_train * val_split)
+	num_train -= num_val
+
+	# Diviser le dataset en ensembles d'entraînement et de validation
+	train_dataset, val_dataset = torch.utils.data.random_split(full_train_dataset, [num_train, num_val])
+
+	# Créer les DataLoaders
+	batch_size = 5  # Assurez-vous que ce paramètre est défini ailleurs dans votre code
+
 	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+	test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(data_test, label_test), batch_size=1, shuffle=False)
+
+	# Définir le pourcentage pour la validation
+	val_split = 0.2
+	num_train = len(full_train_dataset)
+	num_val = int(num_train * val_split)
+	num_train -= num_val
+
+	## on lit les données
+	#((data_train,label_train),(data_test,label_test)) = torch.load(gzip.open('mnist.pkl.gz'))
+	## on crée les lecteurs de données
+	#train_dataset = torch.utils.data.TensorDataset(data_train,label_train)
+	#test_dataset = torch.utils.data.TensorDataset(data_test,label_test)
+	#train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+	#test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 	# on initialise le modèle et ses poids
 	# model = torch.nn.Linear(data_train.shape[1],label_train.shape[1])
@@ -50,6 +83,10 @@ def train_return(batch_size = 5, nb_epochs = 3, eta = 0.00001, hidden_neurons = 
 	#train_losses = []
 	test_accuracies = []
 
+    # Mesurer le temps d'entraînement
+	start_time = time.time()
+
+ 
 	for n in range(nb_epochs):
 		# on lit toutes les données d'apprentissage
 		for x,t in train_loader:
@@ -72,21 +109,31 @@ def train_return(batch_size = 5, nb_epochs = 3, eta = 0.00001, hidden_neurons = 
 		# on affiche le pourcentage de bonnes réponses
 		test_accuracies.append((acc/data_test.shape[0]).item())
 		print(acc/data_test.shape[0])
-	return test_accuracies
+  
+    # Calculer le temps écoulé
+	elapsed_time = time.time() - start_time
+
+	return test_accuracies, elapsed_time
+
 
 
     
 if __name__ == '__main__':
 	nb_epochs = 10
-	for i in [32, 128] : 
+	batch_size = 5
+	plt.figure(figsize=(6, 6))
+	c = {16:'red', 32:'green', 64:'yellow', 128:'blue', 256:'grey', 512:'black'}
+	for i in [16, 32, 64, 128, 256, 512] : 
 		print(i)
-		test_accuracies = train_return(5, 10, 0.001, i)
+		test_accuracies, elapsed_time = train_return(batch_size, nb_epochs, 0.001, i)
 		# Visualisation des résultats
-		plt.figure(figsize=(6, 6))
+		
 
-		print(test_accuracies)
+
+  
+  
 		# Graphe de l'accuracy
-		plt.plot(range(1, nb_epochs+1), test_accuracies, label=f'Test Accuracy : {i}', color='green')
+		plt.plot(range(1, nb_epochs+1), test_accuracies, label=f'Nombre de neurones : {i} a duré {elapsed_time:.2f}s', color=c[i])
 
 
 	# Affichage des hyperparamètres choisis
